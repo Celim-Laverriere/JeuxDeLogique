4d3b4eb57a80d9c7c4a23d46dd6fda6f7b3a540c
Celim Laverriere <laverriere.celim@gmail.com>
2017-12-13 16:55:57 +0100

    Création du package "fr.jeuxdelogique.startjeux"
    Descriptif : 
    
    Vous trouverez initialisées dans ce package les classes suivantes :
    - La classe MainJeux qui est ma porte d'entrée et de sortie de mon
    programme.
    - La classe Jeux qui est la classe mère des classes Mastermind et
    RecherchePlusMoins.

---
+++ JeuxDeLogique/src/fr/jeuxdelogique/startjeux/Jeux.java
@@ -0,0 +1,5 @@
+package fr.jeuxdelogique.startjeux;
+
+public class Jeux {
+
+}
---
+++ JeuxDeLogique/src/fr/jeuxdelogique/startjeux/MainJeux.java
@@ -0,0 +1,10 @@
+package fr.jeuxdelogique.startjeux;
+
+public class MainJeux {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
